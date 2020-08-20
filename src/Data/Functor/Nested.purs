e4e642ce92821d7c97e130172dcd7dab9c2e8a51
Mason Mackaman <masondeanm@aol.com>
2020-08-19 21:58:42 -0400

    Use the new combinators

--- src/Data/Functor/Nested.purs
+++ src/Data/Functor/Nested.purs
@@ -1,13 +1,14 @@
 module Data.Functor.Nested where
 
 import Prelude
+import Data.Function.Combinators
 
 mmap :: ∀ a b f g. Functor f => Functor g => (a -> b) -> f (g a) -> f (g b)
-mmap = map <<< map
+mmap = map <. map
 
 infixl 4 mmap as <$$>
 
 mmmap :: ∀ a b f g h. Functor f => Functor g => Functor h => (a -> b) -> f (g (h a)) -> f (g (h b))
-mmmap = map <<< map <<< map
+mmmap = map <. map <. map
 
 infixl 4 mmap as <$$$>
