b9c680e84a277530fd73215f565d414039f4b268
Mason Mackaman <masondeanm@aol.com>
2020-08-19 21:59:36 -0400

    Add the flipped versions

--- src/Data/Functor/Nested.purs
+++ src/Data/Functor/Nested.purs
@@ -8,7 +8,17 @@ mmap = map <. map
 
 infixl 4 mmap as <$$>
 
+mmapFlipped :: ∀ a b f g. Functor f => Functor g => f (g a) -> (a -> b) -> f (g b)
+mmapFlipped = flip mmap
+
+infixl 1 mmapFlipped as <##>
+
 mmmap :: ∀ a b f g h. Functor f => Functor g => Functor h => (a -> b) -> f (g (h a)) -> f (g (h b))
 mmmap = map <. map <. map
 
 infixl 4 mmap as <$$$>
+
+mmmapFlipped :: ∀ a b f g h. Functor f => Functor g => Functor h => f (g (h a)) -> (a -> b) -> f (g (h b))
+mmmapFlipped = flip mmmap
+
+infixl 1 mmmapFlipped as <###>
