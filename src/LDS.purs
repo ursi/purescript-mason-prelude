40a0de2f5f4995430b7868e0a4969abf7c420ebf
Mason Mackaman <masondeanm@aol.com>
2020-08-22 14:04:25 -0400

    Add uncons

--- src/LDS.purs
+++ src/LDS.purs
@@ -1,13 +1,17 @@
 --| Linear Data Structure
 module LDS where
 
+import Prelude
 import Data.Array as Array
-import Data.Maybe (Maybe)
+import Data.Function.Combinators
+import Data.Maybe (Maybe(..))
 import Data.List (List(..))
 import Data.List as List
+import Data.Tuple.Nested (type (/\), (/\))
 
 class LDS l where
   cons :: ∀ a. a -> l a -> l a
+  uncons :: ∀ a. l a -> Maybe (a /\ l a)
   index :: ∀ a. l a -> Int -> Maybe a
   range :: Int -> Int -> l Int
 
@@ -19,10 +23,12 @@ infixr 6 cons as :
 
 instance ldsArray :: LDS Array where
   cons = Array.cons
+  uncons = Array.uncons .> map \{ head, tail } -> head /\ tail
   index = Array.index
   range = Array.range
 
 instance ldsList :: LDS List where
   cons = Cons
+  uncons = List.uncons .> map \{ head, tail } -> head /\ tail
   index = List.index
   range = List.range
