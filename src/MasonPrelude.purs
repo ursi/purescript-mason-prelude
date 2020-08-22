efeee50309b85ec4ff352fbfd899eb3e8fdf9048
Mason Mackaman <masondeanm@aol.com>
2020-08-22 14:11:59 -0400

    Remove LDS

--- src/MasonPrelude.purs
+++ src/MasonPrelude.purs
@@ -21,7 +21,6 @@ import Data.Tuple.Nested (type (/\), (/\)) as Exports
 import Data.Unfoldable (class Unfoldable, unfoldr) as Exports
 import Debug (todo) as Exports
 import Effect (Effect) as Exports
-import LDS as Exports
 import Math (pow)
 
 infixl 7 mod as %
