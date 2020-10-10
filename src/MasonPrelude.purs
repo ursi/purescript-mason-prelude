b0beae680b25b8cfeeec1e048a7b41ed448beabf
Mason Mackaman <masondeanm@aol.com>
2020-10-10 03:01:36 -0400

    Spread long imports over multiple lines

--- src/MasonPrelude.purs
+++ src/MasonPrelude.purs
@@ -8,8 +8,24 @@ import Prelude as Exports
 import Prelude (mod)
 import Data.Either (Either(..)) as Exports
 import Data.Either.Nested (type (\/)) as Exports
-import Data.Foldable (class Foldable, fold, foldl, foldMap, foldr, intercalate, sequence_, traverse_) as Exports
-import Data.FoldableWithIndex (class FoldableWithIndex, foldlWithIndex, foldMapWithIndex, foldrWithIndex) as Exports
+import Data.Foldable
+  ( class Foldable
+  , fold
+  , foldl
+  , foldMap
+  , foldr
+  , intercalate
+  , sequence_
+  , traverse_
+  )
+  as Exports
+import Data.FoldableWithIndex
+  ( class FoldableWithIndex
+  , foldlWithIndex
+  , foldMapWithIndex
+  , foldrWithIndex
+  )
+  as Exports
 import Data.Function.Uncurried as Exports
 import Data.Functor.Nested as Exports
 import Data.FunctorWithIndex (class FunctorWithIndex, mapWithIndex) as Exports
