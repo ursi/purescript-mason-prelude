186ceb912daf157bb0054fb2264f5bf962e0be1a
Mason Mackaman <masondeanm@aol.com>
2021-04-09 14:45:01 -0400

    Update package.nix

--- package.nix
+++ package.nix
@@ -1,8 +1,6 @@
 { ps-pkgs, ps-pkgs-ns, ... }:
   with ps-pkgs;
   { version = "0.7.0";
-    repo = "https://github.com/ursi/purescript-mason-prelude";
-    rev = "1f600849f93b85a38822fd90b4749cb30ab6c4f0";
     dependencies =
       [ arrays
         console
